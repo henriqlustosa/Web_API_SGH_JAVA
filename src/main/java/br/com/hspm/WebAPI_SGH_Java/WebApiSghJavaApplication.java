8b8149e25002d1a7cf28bb4bc428c862b8e1c357
henriqlustosa <henriqlustosa@gmail.com>
2019-08-22 18:30:06 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/hspm/WebAPI_SGH_Java/WebApiSghJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.hspm.WebAPI_SGH_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiSghJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiSghJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hspm/WebAPI_SGH_Java/WebApiSghJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.hspm.WebAPI_SGH_Java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebApiSghJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
